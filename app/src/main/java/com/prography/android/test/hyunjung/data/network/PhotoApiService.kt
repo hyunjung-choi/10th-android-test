8c175fbb5c989bd3fe846388ca7f48838b48fd7d
최현정 <hjchoi.dev@gmail.com>
2025-02-26 12:28:43 +0900

    fix: Modify response

--- app/src/main/java/com/prography/android/test/hyunjung/data/network/PhotoApiService.kt
+++ app/src/main/java/com/prography/android/test/hyunjung/data/network/PhotoApiService.kt
@@ -1,5 +1,6 @@
 package com.prography.android.test.hyunjung.data.network
 
+import android.util.Log
 import com.prography.android.test.hyunjung.BuildConfig
 import com.prography.android.test.hyunjung.data.model.Photo
 import com.prography.android.test.hyunjung.utils.Constants
@@ -7,6 +8,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.headers
+import io.ktor.client.request.parameter
 import io.ktor.client.statement.HttpResponse
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -20,11 +22,14 @@ class PhotoApiService @Inject constructor(
 
     suspend fun getLatestPhotos(page: Int): List<Photo> {
         return try {
-            val response: HttpResponse = client.get("$baseUrl/photos?page=$page") {
+            val response: HttpResponse = client.get("$baseUrl/photos") {
+                parameter("page", page)
                 headers {
                     append("Authorization", "Client-ID $accessKey")
                 }
             }
+            val responseBody = response.body<String>()
+            Log.d("API_RESPONSE", "Response Body: $responseBody")
             response.body()
         } catch (e: Exception) {
             emptyList()
